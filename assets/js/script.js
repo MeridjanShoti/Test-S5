67b92d972f678297aae64c5c431613b965f4f483
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-11 14:40:34 +0100

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ addEventListener("scroll",(event) => {
         document.querySelector(".navbar").classList.remove("changeColor");
     }
 })
+
+let m = document.querySelectorAll('g[transform^="matrix"]')
+console.log(m)
+setInterval(function (){
+let i = 0;
+if (m[i].opacity === 0){
+m[i].opacity = 1}
+else{
+    m[i].opacity = 0
+}
+i++
+}, 1000)
