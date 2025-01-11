7a6821a0d2eccc3208da2b2eaa3b9fb54a489b39
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-11 11:25:11 +0100

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-/* addEventListener("scroll",(event) => {
+addEventListener("scroll",(event) => {
+    if (scrollY >= 527){
     document.querySelector(".navbar").classList.add("changeColor");
-    
-}) */
+    }
+    else{
+        document.querySelector(".navbar").classList.remove("changeColor");
+    }
+})
