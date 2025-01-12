07f8ae01a14adff26d840d77622bc779b41a5311
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-12 15:55:05 +0100

    there is alway something you can do better

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,11 @@ let svg = document.querySelector('.svg svg');
 
 //seleziono tutti gli elementi g che hanno l'attributo opacity
 
-let arrayGroup = [...svg.querySelectorAll('g[opacity]')]
+let arrayGroup = [...svg.querySelectorAll('g[opacity="1"]')]
 
 function randomOpacity(){
-    let index = Math.floor(Math.random()*arrayGroup.length)
+    let index = Math.floor(Math.random()*arrayGroup.length-1)+1
     let gruppo = arrayGroup[index]
-
     gruppo.setAttribute('opacity', 0)
     setTimeout(()=>{
         gruppo.setAttribute('opacity', 1)
